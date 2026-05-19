2baa3aedd71e48460e98217ef6de5fa214be6f97
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-19 23:46:50 +0530

    feat: add membership tier entity

---
+++ src/main/java/com/quickcom/membership/domain/entity/MembershipTier.java
@@ -0,0 +1,26 @@
+package com.quickcom.membership.domain.entity;
+
+import com.quickcom.membership.domain.enums.TierType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "membership_tiers")
+public class MembershipTier extends BaseEntity{
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 50)
+    private TierType tierType;
+
+    @Column(nullable = false, length = 100)
+    private String displayName;
+
+    @Column(nullable = false)
+    private Integer priority;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
