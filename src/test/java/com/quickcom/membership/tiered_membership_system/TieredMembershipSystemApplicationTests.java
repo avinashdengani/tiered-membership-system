e5c540d8db2a648ef8cec07664b26b66e2ca5021
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-19 20:05:52 +0530

    chore: initialize Spring Boot membership engine project

---
+++ src/test/java/com/quickcom/membership/tiered_membership_system/TieredMembershipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickcom.membership.tiered_membership_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TieredMembershipSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
