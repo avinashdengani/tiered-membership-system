bbb81168681278e9911a832842822b8bc3ca9c2b
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-20 00:12:18 +0530

    feat: add configurable tier rule entity

---
+++ src/main/java/com/quickcom/membership/domain/entity/TierRule.java
@@ -0,0 +1,32 @@
+package com.quickcom.membership.domain.entity;
+
+import com.quickcom.membership.domain.enums.OperatorType;
+import com.quickcom.membership.domain.enums.RuleType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tier_rules")
+public class TierRule extends BaseEntity{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tier_id", nullable = false)
+    private MembershipTier tier;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 100)
+    private RuleType ruleType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private OperatorType operatorType;
+
+    @Column(nullable = false)
+    private String thresholdValue;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
