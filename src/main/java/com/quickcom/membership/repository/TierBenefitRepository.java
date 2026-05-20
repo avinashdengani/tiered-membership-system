2ddc64de30a08d7874d25c4bd623c52e680561a7
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-20 21:34:36 +0530

    feat: add tier benefit repository

---
+++ src/main/java/com/quickcom/membership/repository/TierBenefitRepository.java
@@ -0,0 +1,17 @@
+package com.quickcom.membership.repository;
+
+import com.quickcom.membership.domain.entity.TierBenefit;
+import com.quickcom.membership.domain.entity.MembershipTier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TierBenefitRepository
+        extends JpaRepository<TierBenefit, UUID> {
+
+    List<TierBenefit> findAllByTier(
+            MembershipTier tier
+    );
+}
