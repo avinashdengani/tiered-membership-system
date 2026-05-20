b06649b6c3ab30f21c3770b93afdb7d82d2b8faa
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-20 21:15:47 +0530

    feat: add membership plan repository abstraction

---
+++ src/main/java/com/quickcom/membership/repository/MembershipPlanRepository.java
@@ -0,0 +1,18 @@
+package com.quickcom.membership.repository;
+
+import com.quickcom.membership.domain.entity.MembershipPlan;
+import com.quickcom.membership.domain.enums.PlanType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MembershipPlanRepository
+        extends JpaRepository<MembershipPlan, UUID> {
+
+    Optional<MembershipPlan> findByPlanTypeAndActiveTrue(
+            PlanType planType
+    );
+
+}
