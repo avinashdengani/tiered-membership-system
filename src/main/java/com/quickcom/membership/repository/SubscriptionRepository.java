097f1202381f537d2cb93c92d8c192e311183b2d
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-20 21:29:29 +0530

    feat: add subscription repository queries

---
+++ src/main/java/com/quickcom/membership/repository/SubscriptionRepository.java
@@ -0,0 +1,15 @@
+package com.quickcom.membership.repository;
+
+import com.quickcom.membership.domain.entity.Subscription;
+import com.quickcom.membership.domain.enums.SubscriptionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SubscriptionRepository
+        extends JpaRepository<Subscription, UUID> {
+
+    Optional<Subscription> findByUserIdAndStatus(UUID userId, SubscriptionStatus status);
+    boolean existsByUserIdAndStatus(UUID userId, SubscriptionStatus status);
+}
