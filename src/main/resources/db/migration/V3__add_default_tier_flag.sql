68da0cf6d80c78396af1b7ce2a78cb12846ee96e
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-22 22:58:47 +0530

    feat: enforce single default tier using partial unique index

--- src/main/resources/db/migration/V3__add_default_tier_flag.sql
+++ src/main/resources/db/migration/V3__add_default_tier_flag.sql
@@ -1,7 +1,10 @@
 ALTER TABLE membership_tier
     ADD COLUMN default_tier BOOLEAN NOT NULL DEFAULT FALSE;
 
-
 UPDATE membership_tier
 SET default_tier = TRUE
 WHERE tier_type = 'SILVER';
+
+CREATE UNIQUE INDEX uq_default_tier
+    ON membership_tier(default_tier)
+    WHERE default_tier = TRUE;
