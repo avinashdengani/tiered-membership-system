d170fa1ec0e36d455795488aec8280c6faf00825
Avinash Dengani <avinashdengani123@gmail.com>
2026-05-22 23:00:04 +0530

    refactor: add default reason/description of SubscriptionActionType

--- src/main/java/com/quickcom/membership/domain/enums/SubscriptionActionType.java
+++ src/main/java/com/quickcom/membership/domain/enums/SubscriptionActionType.java
@@ -1,10 +1,26 @@
 package com.quickcom.membership.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum SubscriptionActionType {
 
-    SUBSCRIBED,
-    UPGRADED,
-    DOWNGRADED,
-    CANCELLED,
-    RENEWED
+    SUBSCRIBED("Initial tier assignment"),
+
+    UPGRADED("Tier upgraded after evaluation"),
+
+    DOWNGRADED("Tier downgraded after evaluation"),
+
+    CANCELLED("Subscription cancelled"),
+
+    RENEWED("Subscription renewed"),
+
+    EXPIRED("Subscription expired");
+
+    private final String defaultReason;
+
+    SubscriptionActionType(String defaultReason) {
+        this.defaultReason = defaultReason;
+    }
+
 }
